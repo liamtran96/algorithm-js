8be09f3e00d7eedbfc5f3fdb843e6eec849c17d7
Liam Tran <111833625+liamtran96@users.noreply.github.com>
2022-11-27 15:35:47 +0000

    27-11: add another for random number [a,b]

--- random-number/index.js
+++ random-number/index.js
@@ -15,7 +15,8 @@ const randomNumber = (number) => {
 // randomNumber(100)
 
 
-//random a number [a, b] (a < b)
+//random a number [a, b] (a < b) (min => max)
+
 const randomNumberFromAtoB = (a, b) => {
     if (a > b || (typeof a !== "number" && typeof b !== "number")) {
         console.log(-1)
@@ -31,19 +32,46 @@ const randomNumberFromAtoB = (a, b) => {
     }
 
 }
+//another ways
+// example: [10, 100]
+// u need to random a number 0->90 (100-10=90) 
+// and then plus the min number
+const randomNumberInRange = (a, b) => {
+    if (a > b || (typeof a !== "number" && typeof b !== "number")) {
+        console.log(-1)
+        return -1;
+    } else {
+        const random = Math.random();
+        console.log("random", random)
+        // console.log("randomtimeb", randomtimeb)
+        // included a and b
+        const result = Math.round(random * (b - a ) + a);
+        console.log(result)
+        return result;
+    }
 
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
-randomNumberFromAtoB(2, 4)
+}
+
+randomNumberInRange(2,5)
+randomNumberInRange(2,5)
+randomNumberInRange(2,5)
+randomNumberInRange(2,5)
+randomNumberInRange(2,5)
+randomNumberInRange(2,5)
+randomNumberInRange(2,5)
+
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
+// randomNumberFromAtoB(2, 4)
