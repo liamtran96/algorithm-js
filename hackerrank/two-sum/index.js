6c86ca46384be88c0e170dfaa00b406a6c0e7271
Liam Tran <111833625+liamtran96@users.noreply.github.com>
2023-08-06 06:47:48 +0000

    hackerrank :enhancement twoSum with hash map aka object in js

--- hackerrank/two-sum/index.js
+++ hackerrank/two-sum/index.js
@@ -25,4 +25,26 @@ var twoSum = function(nums, target) {
     }
 };
 
+
+//using hash map
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const numIndices = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (numIndices.hasOwnProperty(complement)) {
+            return [numIndices[complement], i];
+        }
+        numIndices[nums[i]] = i;
+    }
+    
+    return []; // If no solution is found
+};
+
+
 // https://leetcode.com/problems/two-sum/description/
